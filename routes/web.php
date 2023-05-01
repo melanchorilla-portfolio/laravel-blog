75458e1c8aeaa6bc3f18c0e0a40a6a046b39aa28
melanchorilla-portfolio <dickykamaludin13@gmail.com>
2023-05-01 11:01:43 +0700

    feat: add controllers and templating dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\{ Auth, Route };
+use App\Http\Controllers\Admin\{CategoryController, TagController, PostController};
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -20,3 +11,14 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware('auth')->group(function () {
+    Route::prefix('admin')->group(function () {
+        Route::get('/', function () {
+            return view('admin.dashboard');
+        });
+        Route::resource('/categories', CategoryController::class);
+        Route::resource('/tags', TagController::class);
+        Route::resource('/posts', PostController::class);
+    });
+});
